39b73e640a6eefcacad3a7b4d8358b1e95a44851
enchevri <enzochevrier@icloud.com>
2025-11-30 15:21:11 +0100

    Update .gitignore and improve UI rendering: add .valgrind and .diff to ignore list, enhance rendering logic in game loop and UI components

--- src/player/game_loop.c
+++ src/player/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 15:38:26 by gabach            #+#    #+#             */
-/*   Updated: 2025/11/30 14:24:38 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/11/30 15:20:27 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,14 @@ int	game_loop(t_game *game)
 		actualize_projectiles(&projectiles, counter);
 		actualize_oponent(&oponents, counter, &projectiles);
 		player_action(ch, &game->player, &projectiles);
-		print_ui(*game, ch);
 		refresh();
-		player_action(ch, &game->player, &projectiles);
 		werase(game_win);
-		wattron(game_win, COLOR_PAIR(1));
-		box(game_win, ACS_VLINE, ACS_HLINE);
-		wattroff(game_win, COLOR_PAIR(1));
 		render_projectiles(&projectiles);
 		render_oponents(&oponents, &projectiles, &game->player);
 		render_obj(game->player.y, game->player.x, game->player.icon);
+		print_ui(*game, ch);
 		print_fps(ch);
-		wrefresh(game_win);
-		counter++;
+		game->player.time = ++counter;
 	}
 	ft_lstclear(&projectiles, free);
 	ft_lstclear(&oponents, free);
