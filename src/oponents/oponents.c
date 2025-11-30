2d67bc16b0a84f19fa1b8418d8a43635f1ee3726
enchevri <enzochevrier@icloud.com>
2025-11-30 14:20:23 +0100

    Refactor code style in oponents.c, game_loop.c, and projectile.c: improve formatting and consistency

--- src/oponents/oponents.c
+++ src/oponents/oponents.c
@@ -6,13 +6,13 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 21:24:20 by gabach            #+#    #+#             */
-/*   Updated: 2025/11/30 10:11:51 by gabach           ###   ########lyon.fr   */
+/*   Updated: 2025/11/30 14:19:43 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "objects.h"
-#include <stdlib.h>
 #include "oponents.h"
+#include <stdlib.h>
 
 void	actualize_oponent(t_list **oponents, int counter, t_list **projectiles)
 {
@@ -28,9 +28,10 @@ void	actualize_oponent(t_list **oponents, int counter, t_list **projectiles)
 	last = NULL;
 	while (current)
 	{
-		oponent = (t_oponent*)current->content;
+		oponent = (t_oponent *)current->content;
 		if ((rand() % 100 + 1) % 20 == 0)
-			throw_projectile((oponent->y), (oponent->x), '-', (oponent->direction), projectiles);
+			throw_projectile((oponent->y), (oponent->x), '-',
+				(oponent->direction), projectiles);
 		if (obj_move(&(oponent->y), &(oponent->x), (oponent->direction)) == -1)
 			current = lstdel_relink(oponents, current, last);
 		else
@@ -49,21 +50,23 @@ void	kill_oponent(t_list **oponents, int row, int col)
 	last = NULL;
 	while (current)
 	{
-		oponent = (t_oponent*)current->content;
+		oponent = (t_oponent *)current->content;
 		if (oponent->x == col && oponent->y == row)
 		{
 			current = lstdel_relink(oponents, current, last);
-			return;
+			return ;
 		}
 		else
+		{
+			last = current;
 			current = current->next;
-		last = current;
+		}
 	}
 }
 
 t_oponent	*new_oponent(int row, int col, char icon, char direction)
 {
-	t_oponent *oponent;
+	t_oponent	*oponent;
 
 	oponent = malloc(sizeof(t_oponent));
 	if (!oponent)
@@ -80,11 +83,11 @@ void	spawn_oponent(t_list **oponents)
 	t_oponent	*oponent;
 	t_list		*list;
 
-	//oponent = new_oponent(((rand() % (END_LINE - START_LINE)) + START_LINE), END_COL, '&', 'l');
-	oponent = new_oponent(1, 25, '&', 'l');
+	oponent = new_oponent(((rand() % (END_LINE - START_LINE)) + START_LINE),
+			END_COL, '&', 'l');
 	if (!oponent)
 		return ;
-	list = ft_lstnew((void*)oponent);
+	list = ft_lstnew((void *)oponent);
 	if (!list)
 	{
 		free(oponent);
@@ -93,7 +96,8 @@ void	spawn_oponent(t_list **oponents)
 	ft_lstadd_back(oponents, list);
 }
 
-void	render_oponents(t_list **oponents, t_list **projectiles, t_player *player)
+void	render_oponents(t_list **oponents, t_list **projectiles,
+		t_player *player)
 {
 	t_list		*last;
 	t_list		*current;
@@ -105,15 +109,17 @@ void	render_oponents(t_list **oponents, t_list **projectiles, t_player *player)
 	last = NULL;
 	while (current)
 	{
-		oponent = (t_oponent*)current->content;
-		if (render_obj(oponent->y, oponent->x, oponent->icon) == - 1)
+		oponent = (t_oponent *)current->content;
+		if (render_obj(oponent->y, oponent->x, oponent->icon) == -1)
 		{
-			current = lstdel_relink(oponents, current, last);
 			kill_projectile(projectiles, oponent->y, oponent->x);
+			current = lstdel_relink(oponents, current, last);
 			(player->score)++;
 		}
 		else
+		{
+			last = current;
 			current = current->next;
-		last = current;
+		}
 	}
 }
--- src/projectile/projectile.c
+++ src/projectile/projectile.c
@@ -6,30 +6,31 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 17:45:38 by gabach            #+#    #+#             */
-/*   Updated: 2025/11/30 10:04:27 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/11/30 14:19:57 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "player.h"
 #include "objects.h"
-#include <stdlib.h>
+#include "player.h"
 #include "projectile.h"
+#include <stdlib.h>
 
 void	actualize_projectiles(t_list **projectiles, int counter)
 {
-	t_list		*last;
-	t_list		*current;
+	t_list			*last;
+	t_list			*current;
 	t_projectile	*projectile;
 
 	if (*projectiles == NULL || !projectiles || counter % 2 == 0)
 		return ;
 	current = *projectiles;
 	last = NULL;
-	projectile = (t_projectile*)current->content;
+	projectile = (t_projectile *)current->content;
 	while (current)
 	{
-		projectile = (t_projectile*)current->content;
-		if (obj_move(&(projectile->y), &(projectile->x), (projectile->direction)) == -1)
+		projectile = (t_projectile *)current->content;
+		if (obj_move(&(projectile->y), &(projectile->x),
+				(projectile->direction)) == -1)
 			current = lstdel_relink(projectiles, current, last);
 		else
 			current = current->next;
@@ -39,29 +40,31 @@ void	actualize_projectiles(t_list **projectiles, int counter)
 
 void	kill_projectile(t_list **projectiles, int row, int col)
 {
-	t_list		*last;
-	t_list		*current;
+	t_list			*last;
+	t_list			*current;
 	t_projectile	*projectile;
 
 	current = *projectiles;
 	last = NULL;
 	while (current)
 	{
-		projectile = (t_projectile*)current->content;
+		projectile = (t_projectile *)current->content;
 		if (projectile->x == col && projectile->y == row)
 		{
 			current = lstdel_relink(projectiles, current, last);
-			return;
+			return ;
 		}
 		else
+		{
+			last = current;
 			current = current->next;
-		last = current;
+		}
 	}
 }
 
 t_projectile	*new_projectile(int row, int col, char icon, char direction)
 {
-	t_projectile *projectile;
+	t_projectile	*projectile;
 
 	projectile = malloc(sizeof(t_projectile));
 	if (!projectile)
@@ -73,15 +76,16 @@ t_projectile	*new_projectile(int row, int col, char icon, char direction)
 	return (projectile);
 }
 
-void	throw_projectile(int row, int column, char icon, char direction, t_list **projectiles)
+void	throw_projectile(int row, int column, char icon, char direction,
+		t_list **projectiles)
 {
 	t_projectile	*projectile;
-	t_list		*list;
+	t_list			*list;
 
 	projectile = new_projectile(row, column, icon, direction);
 	if (!projectile)
 		return ;
-	list = ft_lstnew((void*)projectile);
+	list = ft_lstnew((void *)projectile);
 	if (!list)
 	{
 		free(projectile);
@@ -92,8 +96,8 @@ void	throw_projectile(int row, int column, char icon, char direction, t_list **p
 
 void	render_projectiles(t_list **projectiles)
 {
-	t_list		*last;
-	t_list		*current;
+	t_list			*last;
+	t_list			*current;
 	t_projectile	*projectile;
 
 	if (!projectiles || *projectiles == NULL)
@@ -102,11 +106,13 @@ void	render_projectiles(t_list **projectiles)
 	last = NULL;
 	while (current)
 	{
-		projectile = (t_projectile*)current->content;
-		if (render_obj(projectile->y, projectile->x, projectile->icon) == - 1)
+		projectile = (t_projectile *)current->content;
+		if (render_obj(projectile->y, projectile->x, projectile->icon) == -1)
 			current = lstdel_relink(projectiles, current, last);
 		else
+		{
+			last = current;
 			current = current->next;
-		last = current;
+		}
 	}
 }
